30d500269f896ddb7adccbc7cc24d8ccf6c75e28
Sanjeet Kumar <114727526+Sanjeetsuman@users.noreply.github.com>
2023-09-16 23:19:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function submitForm() {
+    // Get the input values from the form
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    
+    // Do something with the input values
+    console.log('Name:', name);
+    console.log('Email:', email);
+  }
+  
